019853f119a9038958617db6c9d3b5dfe64cce59
Hao Lam <lamtuhao98@gmail.com>
2021-03-24 12:08:06 -0700

    testing

--- app/src/main/java/com/example/schoolqa/ComposeActivity.java
+++ app/src/main/java/com/example/schoolqa/ComposeActivity.java
@@ -10,5 +10,6 @@ public class ComposeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        // test
     }
 }
