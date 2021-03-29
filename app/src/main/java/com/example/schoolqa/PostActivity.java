2ed57cd0bc7c41cb9e5345756ed6f71397b75b8a
Hao Lam <lamtuhao98@gmail.com>
2021-03-29 15:21:24 -0700

    Add icons and design layouts for activities: Intro,Login,Register,Search,Profile,Post.

--- app/src/main/java/com/example/schoolqa/PostActivity.java
+++ app/src/main/java/com/example/schoolqa/PostActivity.java
@@ -1,14 +1,50 @@
 package com.example.schoolqa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PostActivity extends AppCompatActivity {
 
+    public static String tag = "PostActivity";
+    TextView tv_title;
+    TextView tv_question_content;
+    TextView tv_author_name; //name of user who created the post
+    EditText et_user_comment;
+    ImageView iv_question_image;
+    RecyclerView recyclerView_post_comments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        //link layout components
+        tv_title = findViewById(R.id.tv_post_title);
+        tv_question_content = findViewById(R.id.tv_post_questionContent);
+        tv_author_name = findViewById(R.id.tv_post_author_name);
+        iv_question_image = findViewById(R.id.iv_post_image);
+        et_user_comment = findViewById(R.id.et_post_userComment);
+        recyclerView_post_comments = findViewById(R.id.rv_post_comments);
+
     }
+
+    public void handle_send_button(View view) {
+        //Send button clicked
+        Log.d(tag,"Send button clicked");
+
+        //get user comment & add comment to Post's list of comments
+
+    }
+    public void handle_back_button(View view) {
+        //Back button clicked
+        Log.d(tag,"Back button clicked");
+        finish(); //go back to previous screen - Search screen/Profile screen
+    }
+
+
 }
--- app/src/main/java/com/example/schoolqa/RegisterActivity.java
+++ app/src/main/java/com/example/schoolqa/RegisterActivity.java
@@ -3,12 +3,46 @@ package com.example.schoolqa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static String tag = "RegisterActivity";
+    EditText et_username;
+    EditText et_password;
+    EditText et_email;
+    EditText et_major; //user's major/profession
+    EditText et_year; //user's year of experience / year of graduation
+    EditText et_intro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        //link layout components
+          et_username= findViewById(R.id.et_signup_username);
+          et_password= findViewById(R.id.et_signup_password);
+          et_email= findViewById(R.id.et_signup_email);
+          et_major= findViewById(R.id.et_signup_major);
+          et_year= findViewById(R.id.et_signup_yearGraduate);
+          et_intro = findViewById(R.id.et_signup_intro);
+
+    }
+
+    public void handle_signUp(View view) {
+        //Sign up button clicked
+        Log.d(tag,"Signup button clicked");
+        //Register account
+
+        //Log-in user
+
+    }
+
+    public void handle_cancel(View view) {
+        //Cancel button clicked
+        Log.d(tag,"Cancel button clicked");
+        finish();
     }
 }
