a223b3300a154e45fa3d5e5b6c1a944463c5159b
Hao Lam <lamtuhao98@gmail.com>
2021-04-19 10:59:57 -0700

    capture photo feature for Comment in Post activity

--- app/src/main/java/com/example/schoolqa/PostActivity.java
+++ app/src/main/java/com/example/schoolqa/PostActivity.java
@@ -1,15 +1,24 @@
 package com.example.schoolqa;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,26 +35,34 @@ import com.parse.ParseUser;
 
 import org.parceler.Parcels;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PostActivity extends AppCompatActivity {
 
     public static String tag = "PostActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 45;
     TextView tv_title;
     TextView tv_question_content;
     TextView tv_author_name; //name of user who created the post
     TextView tv_post_timestamp;
+    TextView tv_imageAttached;
     EditText et_user_comment;
+    ImageView iv_comment_image;
     ImageView iv_question_image;
     TextView btnn_vote;
+    ImageButton bttn_cancel_comment_image;
     ProgressBar progressBar;
     RecyclerView recyclerView_post_comments;
     CommentAdapter commentAdapter;
     int vote;
     Boolean is_upvote;
+    Boolean comment_image_exist;
     Post post;
     List<Comment> commentList;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,13 +73,17 @@ public class PostActivity extends AppCompatActivity {
         tv_question_content = findViewById(R.id.tv_post_questionContent);
         tv_author_name = findViewById(R.id.tv_post_author_name);
         tv_post_timestamp = findViewById(R.id.tv_post_timestamp);
+        tv_imageAttached = findViewById(R.id.imageAttached);
         iv_question_image = findViewById(R.id.iv_post_image);
+        iv_comment_image = findViewById(R.id.iv_post_comment_image);
         et_user_comment = findViewById(R.id.et_post_userComment);
         btnn_vote = findViewById(R.id.bttn_post_upvote);
+        bttn_cancel_comment_image = findViewById(R.id.bttn_post_cancle_comment_image);
         progressBar  = findViewById(R.id.post_progressBar);
         recyclerView_post_comments = findViewById(R.id.rv_post_comments);
 
         is_upvote = false;
+        comment_image_exist = false;
 
         post = (Post) Parcels.unwrap(getIntent().getParcelableExtra("post"));
 
@@ -71,13 +92,18 @@ public class PostActivity extends AppCompatActivity {
         recyclerView_post_comments.setAdapter(commentAdapter);
         recyclerView_post_comments.setLayoutManager(new LinearLayoutManager(this));
 
-
+        //set visibility of progress bar
         progressBar.setVisibility(View.VISIBLE);
         setupPost(post);
         progressBar.setVisibility(View.INVISIBLE);
     }
 
     private void setupPost(Post post) {
+        //set visibility of comment image & text & button
+        iv_comment_image.setVisibility(View.INVISIBLE);
+        tv_imageAttached.setVisibility(View.INVISIBLE);
+        bttn_cancel_comment_image.setVisibility(View.INVISIBLE);
+        //set text & image view
         tv_title.setText(post.getQuestion());
         tv_question_content.setText(post.getContent());
         tv_author_name.setText(post.getUser().getUsername());
@@ -137,12 +163,21 @@ public class PostActivity extends AppCompatActivity {
         comment.setContent(input);
         comment.setUser(ParseUser.getCurrentUser());
         comment.setPostId(post.getObjectId());
+        if (comment_image_exist ==true) {
+            comment.setImage(new ParseFile(photoFile));
+            comment_image_exist = false; //reset
+        }
         comment.saveInBackground();// Immediately save the data asynchronously
         Log.d(tag,"Send button clicked - comment: "+input);
 
         Log.d(tag,"Update comment list ");
-        queryComments();
+        //when sent...reset text box & image view
+        iv_comment_image.setVisibility(View.INVISIBLE);
+        tv_imageAttached.setVisibility(View.INVISIBLE);
+        bttn_cancel_comment_image.setVisibility(View.INVISIBLE);
         et_user_comment.setText("");
+
+        queryComments();
         Toast.makeText(this,"Comment sent!", Toast.LENGTH_SHORT).show();
 
 
@@ -170,6 +205,75 @@ public class PostActivity extends AppCompatActivity {
         btnn_vote.setText(String.valueOf(vote));
         post.saveInBackground();// Immediately save the data asynchronously
 
+    }
+
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), tag);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(tag, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(PostActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                iv_comment_image.setImageBitmap(takenImage);
+                iv_comment_image.setVisibility(View.VISIBLE);
+                tv_imageAttached.setVisibility(View.VISIBLE);
+                bttn_cancel_comment_image.setVisibility(View.VISIBLE);
+                comment_image_exist = true;
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void handle_add_image(View view) {
+        launchCamera();
+
+
+    }
+    public void handle_cancel_comment_image(View view) {
+        iv_comment_image.setVisibility(View.INVISIBLE);
+        tv_imageAttached.setVisibility(View.INVISIBLE);
+        bttn_cancel_comment_image.setVisibility(View.INVISIBLE);
+        comment_image_exist = false;
     }
     public void handle_back_button(View view) {
         //Back button clicked
