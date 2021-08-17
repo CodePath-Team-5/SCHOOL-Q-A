53677e662642bd61405d14d6cc10566853bc6ee5
Hao Lam <lamtuhao98@gmail.com>
2021-08-17 21:02:46 +0700

    fix color layout 2 & delete GIF button

--- app/src/main/java/com/example/schoolqa/PostActivity.java
+++ app/src/main/java/com/example/schoolqa/PostActivity.java
@@ -609,17 +609,17 @@ public class PostActivity extends AppCompatActivity implements CommentAdapter.On
     }
 
  */
-    public void insert_image(View view) {
-    // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, PICK_IMAGE);
-        }
-    }
+//    public void insert_image(View view) {
+//    // Create intent for picking a photo from the gallery
+//        Intent intent = new Intent(Intent.ACTION_PICK,
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+//        // So as long as the result is not null, it's safe to use the intent.
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            // Bring up gallery to select a photo
+//            startActivityForResult(intent, PICK_IMAGE);
+//        }
+//    }
 
 
 }
